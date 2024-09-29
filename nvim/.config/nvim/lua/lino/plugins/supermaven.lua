1f4fd91db50334342b1622f8e61a86940458f040
YUAN Lijun <yuan.lijun@hotmail.com>
2024-09-29 09:48:21 +0800

    feat(nvim): hack supermaven highlight

--- nvim/.config/nvim/lua/lino/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/lino/plugins/supermaven.lua
@@ -10,5 +10,13 @@ return {
       },
       log_level = "off",
     },
+    -- NOTE: hack the suggestion_group highlight, as the plugin doesn't support lazy loading yet
+    config = function(_, opts)
+      require("supermaven-nvim").setup(opts)
+
+      local preview = require("supermaven-nvim.completion_preview")
+      vim.api.nvim_set_hl(0, "SupermavenSuggestion", { fg = "#c6a0f6", italic = true })
+      preview.suggestion_group = "SupermavenSuggestion"
+    end,
   },
 }
