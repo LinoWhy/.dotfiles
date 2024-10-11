21dd46f71a2e40ae1c32fcbccb3c0f137361f569
YUAN Lijun <yuan.lijun@hotmail.com>
2024-10-11 19:27:01 +0800

    feat(nvim): enable flash

--- nvim/.config/nvim/lua/lino/plugins/flash.lua
+++ nvim/.config/nvim/lua/lino/plugins/flash.lua
@@ -1,6 +1,5 @@
 return {
   "folke/flash.nvim",
-  enabled = false,
   event = { "BufRead", "BufNewFile" },
   -- stylua: ignore
   keys = { { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" } },
