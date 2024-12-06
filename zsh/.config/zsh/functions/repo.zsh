3e5cc36463ddd291cf326ccb9d18665223c0084f
YUAN Lijun <yuan.lijun@hotmail.com>
2024-12-06 11:35:25 +0800

    feat(repo): refine repo project branch status

--- zsh/.config/zsh/functions/repo.zsh
+++ zsh/.config/zsh/functions/repo.zsh
@@ -5,7 +5,11 @@ function rstt() {
 
 # check repo project branch status
 function rsbb() {
-  repo forall -pc git branch -vv | rg "behind|ahead|change-" -B1
+  repo forall -c bash -c \
+    '[[ $(git branch | wc -l) -gt 1 \
+    || -z $(git branch --show-current) \
+    || $(git status -sb | rg "ahead|behind" &>/dev/null; echo $?) -eq 0 ]] && \
+      echo -e "\n\033[1;31mproject $REPO_PATH/\033[0m" && git branch -vv'
 }
 
 # fetch repo project and check branch status
