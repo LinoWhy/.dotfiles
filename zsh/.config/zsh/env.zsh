d83e7e385f209868ec58000b7536749fc8ca2ec6
YUAN Lijun <yuan.lijun@hotmail.com>
2025-02-03 23:36:06 +0800

    feat(wsl): update wsl proxy settings

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -9,28 +9,10 @@ if isWsl; then
   alias start='wslview'
 fi
 
-# add proxy
-# set "nameserver 8.8.8.8" in /etc/resolv.conf?
-if isWsl; then
-  export hostip=$(ip route | grep default | awk '{print $3}')
-  export socks_hostport=10810 # v2ray
-  export http_hostport=10811 #v2ray
-  alias proxy='
-    export https_proxy="http://${hostip}:${http_hostport}"
-    export http_proxy="http://${hostip}:${http_hostport}"
-    export ALL_PROXY="socks5://${hostip}:${socks_hostport}"
-    export all_proxy="socks5://${hostip}:${socks_hostport}"
-  '
-  alias unproxy='
-    unset ALL_PROXY
-    unset https_proxy
-    unset http_proxy
-    unset all_proxy
-  '
-  alias echoproxy='
-    echo $ALL_PROXY
-    echo $all_proxy
-    echo $https_proxy
-    echo $http_proxy
-  '
-fi
+## add following lines to /mnt/c/Users/<useranme>/.wslconfig
+# [experimental]
+# autoMemoryReclaim=gradual
+# networkingMode=mirrored
+# dnsTunneling=true
+# firewall=true
+# autoProxy=true
