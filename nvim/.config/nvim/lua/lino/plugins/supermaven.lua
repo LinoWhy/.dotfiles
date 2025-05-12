e500d54e20a69ad0ec49eab66401b6c5044cffda
YUAN Lijun <yuan.lijun@hotmail.com>
2025-05-12 09:43:35 +0800

    feat(nvim): Accept word completions from supermaven

--- nvim/.config/nvim/lua/lino/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/lino/plugins/supermaven.lua
@@ -8,5 +8,12 @@ return {
       disable_inline_completion = true,
       disable_keymaps = true,
     },
+    config = function(_, opts)
+      require("supermaven-nvim").setup(opts)
+
+      -- add keymap to accept word completions, other keymaps are managed by nvim-cmp
+      local completion_preview = require("supermaven-nvim.completion_preview")
+      vim.keymap.set("i", "<A-w>", completion_preview.on_accept_suggestion_word, { noremap = true, silent = true })
+    end,
   },
 }
