0d7a1dce1b61443ba9856bb848cda5b2348781ac
YUAN Lijun <yuan.lijun@hotmail.com>
2026-01-28 10:16:49 +0800

    feat(nvim): add im-select again

---
+++ nvim/.config/nvim/lua/lino/plugins/im-select.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "keaising/im-select.nvim",
+    -- event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("im_select").setup({
+        keep_quiet_on_no_binary = true,
+        async_switch_im = false,
+      })
+    end,
+  },
+}
