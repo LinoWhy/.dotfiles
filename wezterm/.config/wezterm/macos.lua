38497a53102199215aba673cabd2d749a1f86935
YUAN Lijun <yuan.lijun@hotmail.com>
2026-02-02 09:36:40 +0800

    fix(wezterm): avoid font changed slightly on selection

--- wezterm/.config/wezterm/macos.lua
+++ wezterm/.config/wezterm/macos.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 
 function M.setup(config)
   config.font_size = 14
+  config.freetype_load_flags = "NO_HINTING" -- avoid font changed slightly on selection
   config.window_decorations = "RESIZE"
   config.native_macos_fullscreen_mode = true
 end
