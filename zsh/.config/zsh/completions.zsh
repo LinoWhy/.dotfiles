e34ad7a9f6c1e067bec93e08ec407b0615a56c88
YUAN Lijun <yuan.lijun@hotmail.com>
2026-02-28 10:02:05 +0800

    feat(zsh): sort zsh completions path

--- zsh/.config/zsh/completions.zsh
+++ zsh/.config/zsh/completions.zsh
@@ -1,5 +1,10 @@
-# Set completion path
-fpath=($ZDOTDIR/completions $fpath)
+# Completion path priority:
+# 1) Homebrew completions
+# 2) Personal completions
+typeset -gU fpath
+fpath=("$ZDOTDIR/completions" $fpath)
+[[ -n "${HOMEBREW_PREFIX:-}" && -d "${HOMEBREW_PREFIX}/share/zsh/site-functions" ]] && \
+  fpath=("${HOMEBREW_PREFIX}/share/zsh/site-functions" $fpath)
 
 # case insensitive completion, partial-word with [._-] and substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
