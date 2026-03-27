f64c6a07c4a6addba5580d0c6f6a44a9d5cde35f
YUAN Lijun <yuan.lijun@hotmail.com>
2026-03-27 16:43:30 +0800

    chore: update cargo ignore & fix key mapping

--- nvim/.config/nvim/lua/lino/plugins/meow.yarn.lua
+++ nvim/.config/nvim/lua/lino/plugins/meow.yarn.lua
@@ -2,8 +2,8 @@ return {
   "retran/meow.yarn.nvim",
   cmd = { "MeowYarn" },
   keys = {
-    { "<leader>lc", "<cmd>MeowYarn call callers<cr>", desc = "Type Hierarchy" },
-    { "<leader>lt", "<cmd>MeowYarn type super<cr>", desc = "Call Hierarchy" },
+    { "<leader>lc", "<cmd>MeowYarn call callers<cr>", desc = "Call Hierarchy" },
+    { "<leader>lt", "<cmd>MeowYarn type super<cr>", desc = "Type Hierarchy" },
   },
   dependencies = { "MunifTanjim/nui.nvim" },
   config = function()
